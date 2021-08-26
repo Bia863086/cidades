e7d029c2d39aaaaa1b7c73f404e75ae28e650e58
Bia863086 <beatrizcampos2101@gmail.com>
2021-08-26 16:40:05 -0300

    Nomes das cidades do litoral Sul

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peruíbe");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Santos");
+        }
+    }
+}
